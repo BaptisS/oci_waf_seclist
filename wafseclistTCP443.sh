7b4fd8447ea1a906a1dac78f772469b92e599bdf
BaptisS <56864846+BaptisS@users.noreply.github.com>
2020-05-21 20:50:38 +0200

    Update wafseclistTCP443.sh

--- wafseclistTCP443.sh
+++ wafseclistTCP443.sh
@@ -4,6 +4,7 @@ chmod +x wafrule-TCP443.sh
 wafips=$(oci waas edge-subnet list --all)
 wafcidrs=$(echo $wafips | jq '.data[] | .cidr')
 rm -f seclist-waf-TCP443.json
+rm -f seclist-waf-TCP443_fixed.json
 echo "[" >> seclist-waf-TCP443.json
 for cidr in $wafcidrs; do ./wafrule-TCP443.sh $cidr ; done
 echo "]" >> seclist-waf-TCP443.json
@@ -12,3 +13,4 @@ sed -zr 's/,([^,]*$)/\1/' seclist-waf-TCP443.json > seclist-waf-TCP443_fixed.jso
 oci network security-list update --security-list-id $wafseclist --ingress-security-rules file://seclist-waf-TCP443_fixed.json --force
 rm -f seclist-waf-TCP443.json
 rm -f seclist-waf-TCP443_fixed.json
+rm -f wafrule-TCP443.sh
