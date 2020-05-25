5e8fd511c9cfd1f480196016c2b711d9b171d53d
BaptisS <56864846+BaptisS@users.noreply.github.com>
2020-05-25 10:24:13 +0200

    Update wafrule-TCP80443.sh

--- wafrule-TCP80443.sh
+++ wafrule-TCP80443.sh
@@ -5,7 +5,7 @@ echo "        \"is-stateless\": false,">> seclist-waf-TCP80443-temp.json
 echo "        \"protocol\": \"6\",">> seclist-waf-TCP80443-temp.json
 echo "        \"source\": $1,">> seclist-waf-TCP80443-temp.json
 echo "        \"source-type\": \"CIDR_BLOCK\",">> seclist-waf-TCP80443-temp.json
-echo "        \"tcp-options\": {">> seclist-waf-TCP80443.json
+echo "        \"tcp-options\": {">> seclist-waf-TCP80443-temp.json
 echo "          \"destination-port-range\": {">> seclist-waf-TCP80443-temp.json
 echo "            \"max\": 80,">> seclist-waf-TCP80443-temp.json
 echo "            \"min\": 80">> seclist-waf-TCP80443-temp.json
