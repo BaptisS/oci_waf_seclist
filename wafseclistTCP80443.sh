badc31ed5e35dc68e10e8461bff7bb8d3d132aeb
BaptisS <56864846+BaptisS@users.noreply.github.com>
2020-05-25 10:02:29 +0200

    Update wafseclistTCP80443.sh

--- wafseclistTCP80443.sh
+++ wafseclistTCP80443.sh
@@ -13,6 +13,7 @@ rm -f seclist-waf-TCP80443.json
 echo "[" >> seclist-waf-TCP80443-temp.json
 for cidr in $wafcidrs; do ./wafrule-TCP80443.sh $cidr ; done
 for cidr in $wafcidrs; do ./wafrule-TCP443.sh $cidr ; done
+cat seclist-waf-TCP443.json >> seclist-waf-TCP80443-temp.json
 echo "]" >> seclist-waf-TCP80443-temp.json
 sed -i 's+66.254.103.241+66.254.103.241/32+g' seclist-waf-TCP80443-temp.json                                            
 sed -zr 's/,([^,]*$)/\1/' seclist-waf-TCP80443-temp.json > seclist-waf-TCP80443.json
